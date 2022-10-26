ee4aba08b9663fc527de586ffa7035e70f28c565
esteiner <esteiner@inventage.com>
2022-10-26 18:03:00 +0200

    Refactor run scripts

--- server/src/test/resources/run-keycloak-setup.sh
+++ server/src/test/resources/run-keycloak-setup.sh
@@ -2,33 +2,16 @@
 
 ##########
 #
-# This script takes properties from additional files and sets them as environment variables (for simulating docker-compose with 'env_file' entries)
-# before executing the shell script given by the first argument.
-# The current directory must be `keycloak-server/target/keycloak` (as the WORKDIR in the docker image)
-# Example: run-keycloak.sh keycloak-configuration.sh UAT.env specific.env
+# Run the setup of Keycloak with the environment variable values from the .env files of the docker-compose module.
 #
 #########
 
 set -euo pipefail
 
-echo "$@"
-
-COMMAND=$1
-shift
-
-while [[ $# -gt 0 ]]
-do
-    CONF_FILE=$1
-    shift
-    if [[ -f $CONF_FILE ]]
-    then
-        echo "Loading file $CONF_FILE"
-        export $(grep -v '^#' $CONF_FILE | xargs)
-    else
-        echo "Env file $CONF_FILE does not exist"
-    fi
-done
+export $(grep -v '^#' ./docker-compose/src/main/resources/keycloak.common.env | xargs)
+export $(grep -v '^#' ./docker-compose/src/main/resources/keycloak.specific.env | xargs)
+export $(grep -v '^#' ./docker-compose/src/main/resources/secrets.env | xargs)
 
 # set the working directory as in the docker image
 cd ./server/target/keycloak/
-bash ./bin/$COMMAND
+bash ./bin/keycloak-setup.sh
--- server/src/test/resources/run-keycloak.sh
+++ server/src/test/resources/run-keycloak.sh
@@ -2,34 +2,16 @@
 
 ##########
 #
-# This script takes properties from additional files and sets them as environment variables (for simulating docker-compose with 'env_file' entries)
-# before executing the shell script given by the first argument.
-# The current directory must be `keycloak-server/target/keycloak` (as the WORKDIR in the docker image)
-# Example: run-keycloak.sh keycloak-configuration.sh UAT.env specific.env
+# Start Keycloak with the environment variables defined by the 3 .env files.
 #
 #########
 
 set -euo pipefail
 
-echo "$@"
-
-# first argument is the command for the kc.sh script
-COMMAND=$1
-shift
-
-while [[ $# -gt 0 ]]
-do
-    CONF_FILE=$1
-    shift
-    if [[ -f $CONF_FILE ]]
-    then
-        echo "Loading file $CONF_FILE"
-        export $(grep -v '^#' $CONF_FILE | xargs)
-    else
-        echo "Env file $CONF_FILE does not exist"
-    fi
-done
+export $(grep -v '^#' ./docker-compose/src/main/resources/keycloak.common.env | xargs)
+export $(grep -v '^#' ./docker-compose/src/main/resources/keycloak.specific.env | xargs)
+export $(grep -v '^#' ./docker-compose/src/main/resources/secrets.env | xargs)
 
 # set the working directory as in the docker image
 cd ./server/target/keycloak/
-bash ./bin/kc.sh --debug $COMMAND
+bash ./bin/kc.sh --debug start-dev
