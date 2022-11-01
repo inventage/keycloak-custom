73bad1a9e89fe6a31736d1d3ce470590f0e63931
esteiner <esteiner@inventage.com>
2022-11-01 11:33:57 +0100

    example2 with CLI

--- config/src/main/resources/keycloak/bin/keycloak-cli-custom.sh
+++ config/src/main/resources/keycloak/bin/keycloak-cli-custom.sh
@@ -6,5 +6,6 @@ echo ""
 echo "----------------- keycloak_custom_config.sh ---------------"
 echo ""
 
-# remove the following line if custom configs are added
-echo "no entries"
+createRealm example2
+createClient example2 client1
+createUser example2 user1
