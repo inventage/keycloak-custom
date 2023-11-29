486911619d1871351878345b0971acad9fff3517
Kevin Tang <kevin.tang@inventage.com>
2023-11-29 15:24:08 +0100

    Increase default startup timeout for our custom keycloak testcontainers (keycloak config cli/importing realms takes long)

--- container/src/test/java/utils/KeycloakCustomContainer.java
+++ container/src/test/java/utils/KeycloakCustomContainer.java
@@ -1,21 +1,31 @@
 package utils;
 
 import dasniko.testcontainers.keycloak.ExtendableKeycloakContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 import org.testcontainers.containers.wait.strategy.WaitStrategy;
+
+import java.time.Duration;
 
 public class KeycloakCustomContainer extends ExtendableKeycloakContainer<KeycloakCustomContainer> {
 
     private final WaitStrategy waitStrategy;
+    private final long startupTimeout;
     private static final String DEFAULT_WAIT_LOG_REGEX = ".*KEYCLOAK SETUP FINISHED.*";
-    public KeycloakCustomContainer(String dockerImageName, WaitStrategy waitStrategy) {
+    private static final long DEFAULT_STARTUP_TIMEOUT = 3;
+
+    public KeycloakCustomContainer(String dockerImageName, WaitStrategy waitStrategy, long startupTimeout) {
         super(dockerImageName);
         this.waitStrategy = waitStrategy;
+        this.startupTimeout = startupTimeout;
     }
 
     public KeycloakCustomContainer(String dockerImageName){
         super(dockerImageName);
-        this.waitStrategy = Wait.forLogMessage(DEFAULT_WAIT_LOG_REGEX, 1);
+        this.startupTimeout = DEFAULT_STARTUP_TIMEOUT;
+        this.waitStrategy = new LogMessageWaitStrategy()
+                .withRegEx(DEFAULT_WAIT_LOG_REGEX)
+                .withTimes(1)
+                .withStartupTimeout(Duration.ofMinutes(this.startupTimeout));
     }
     @Override
     protected void configure() {
