0919875cfa05e08705379b07c31bda6b9622f48d
Kevin Tang <kevin.tang@inventage.com>
2023-12-04 11:01:28 +0100

    Integrationtests: Using postgres as db-testcontainer instead of dev-mem

--- container/src/test/java/KeycloakConfigCLITestIT.java
+++ container/src/test/java/KeycloakConfigCLITestIT.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.representations.idm.RealmRepresentation;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import utils.KeycloakCustomContainer;
 
@@ -15,17 +17,33 @@ import java.util.Optional;
 class KeycloakConfigCLITestIT {
 
     private static final String DOCKER_IMAGE_NAME = "docker-registry.inventage.com:10094/com.inventage.keycloak.custom.container:latest";
-    private static final Map<String, String> envMap = Map.of("KC_DB", "dev-mem", "KC_LOG_LEVEL", "info");
-    private static final KeycloakCustomContainer keycloak = new KeycloakCustomContainer(DOCKER_IMAGE_NAME)
-            .withEnv(envMap);
+    private static final String POSTGRES_DOCKER_IMAGE_NAME = "postgres:13-alpine";
+    private static final String POSTGRES_NETWORK_ALIAS = "postgres";
+    private static KeycloakCustomContainer keycloak;
+    private static PostgreSQLContainer<?> postgres;
 
     @BeforeAll
     static void beforeAll() {
+        Network network = Network.newNetwork();
+        final String jdbcUrl = String.format("jdbc:postgresql://%s:5432/test?loggerLevel=OFF", POSTGRES_NETWORK_ALIAS);
+
+        postgres = new PostgreSQLContainer<>(POSTGRES_DOCKER_IMAGE_NAME)
+                .withNetwork(network).withNetworkAliases(POSTGRES_NETWORK_ALIAS);
+        postgres.start();
+
+        keycloak = new KeycloakCustomContainer(DOCKER_IMAGE_NAME)
+                .withNetwork(network)
+                .withEnv(Map.of("KC_DB", "postgres",
+                        "KC_DB_USERNAME" , postgres.getUsername(),
+                        "KC_DB_PASSWORD", postgres.getPassword(),
+                        "KC_DB_URL", jdbcUrl,
+                        "KC_LOG_LEVEL", "info"));
         keycloak.start();
     }
     @AfterAll
     static void afterAll() {
         keycloak.stop();
+        postgres.close();
     }
     @Test
     void test_startup() {
