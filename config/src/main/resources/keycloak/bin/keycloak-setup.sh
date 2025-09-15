f00c7883528e4f5ff36c4452c8cd5ef407a3725a
Florian Bütler <fbuetler@inventage.com>
2025-09-15 15:01:23 +0200

    use consistent naming

--- config/src/main/resources/keycloak/bin/keycloak-setup.sh
+++ config/src/main/resources/keycloak/bin/keycloak-setup.sh
@@ -21,41 +21,46 @@ runKeycloakConfigCli() {
   echo "--- Running Keycloak Config CLI"
   echo ""
 
-  eval "KC_ADMIN_CLIENT_ID=${KEYCLOAK_CLIENT_ID_REF}"
-  eval "KC_ADMIN_CLIENT_SECRET=${KEYCLOAK_CLIENT_SECRET_REF}"
+  eval "ADMIN_CLIENT_ID=${KEYCLOAK_CLIENT_ID_REF}"
+  eval "ADMIN_CLIENT_SECRET=${KEYCLOAK_CLIENT_SECRET_REF}"
 
-  echo "Use keycloak-config-cli with client '${KC_ADMIN_CLIENT_ID}'"
+  echo "Use keycloak-config-cli with client '${ADMIN_CLIENT_ID}'"
   # run keycloak-config-cli
   java -jar "${BASEDIR}"/client/keycloak-config-cli-"${keycloak-config-cli.version}".jar \
-      --keycloak.url=http://localhost:8080/ \
-      --keycloak.ssl-verify=true \
-      --keycloak.grant-type="${KEYCLOAK_GRANT_TYPE}" \
-      --keycloak.client-id="${KC_ADMIN_CLIENT_ID}" \
-      --keycloak.client-secret="${KC_ADMIN_CLIENT_SECRET}" \
-      --keycloak.availability-check.enabled=true \
-      --keycloak.availability-check.timeout=300s \
-      --import.var-substitution.enabled=true \
-      --import.managed.client=no-delete \
-      --import.managed.client-scope=no-delete \
-      --import.managed.client-scope-mapping=no-delete \
-      --import.files.locations="${BASEDIR}"/../setup/*.json \
-      --logging.level.root=info \
-      --logging.level.keycloak-config-cli=info \
-      --logging.level.realm-config=info
+    --keycloak.url=http://localhost:8080/ \
+    --keycloak.ssl-verify=true \
+    --keycloak.grant-type="${KEYCLOAK_GRANT_TYPE}" \
+    --keycloak.client-id="${ADMIN_CLIENT_ID}" \
+    --keycloak.client-secret="${ADMIN_CLIENT_SECRET}" \
+    --keycloak.availability-check.enabled=true \
+    --keycloak.availability-check.timeout=300s \
+    --import.var-substitution.enabled=true \
+    --import.managed.client=no-delete \
+    --import.managed.client-scope=no-delete \
+    --import.managed.client-scope-mapping=no-delete \
+    --import.files.locations="${BASEDIR}"/../setup/*.json \
+    --logging.level.root=info \
+    --logging.level.keycloak-config-cli=info \
+    --logging.level.realm-config=info
 }
 
 runKeycloakCli() {
   if [ "$KCADM" == "" ]; then
       KCADM="${BASEDIR}"/kcadm.sh
-      KCADM_CONFIG="--config /tmp/.keycloak/kcadm.config"
       echo "Using $KCADM as the admin CLI."
   fi
+  KCADM_CONFIG="--config /tmp/.keycloak/kcadm.config" # required to be writable for the current user
 
-  eval "KC_ADMIN_CLIENT_ID=${KEYCLOAK_CLIENT_ID_REF}"
-  eval "KC_ADMIN_CLIENT_SECRET=${KEYCLOAK_CLIENT_SECRET_REF}"
+  eval "ADMIN_CLIENT_ID=${KEYCLOAK_CLIENT_ID_REF}"
+  eval "ADMIN_CLIENT_SECRET=${KEYCLOAK_CLIENT_SECRET_REF}"
 
   echo "Use kcadm with client '${KC_ADMIN_CLIENT_ID}'"
-  ${KCADM} config credentials --server http://localhost:8080 --client ${KC_ADMIN_CLIENT_ID} --secret ${KC_ADMIN_CLIENT_SECRET} --realm master ${KCADM_CONFIG}
+  ${KCADM} config credentials \
+    --server http://localhost:8080 \
+    --client ${ADMIN_CLIENT_ID} \
+    --secret ${ADMIN_CLIENT_SECRET} \
+    --realm master \
+    ${KCADM_CONFIG}
 
   # helper functions using kc admin cli
   source "${BASEDIR}"/keycloak-cli-helpers.sh
